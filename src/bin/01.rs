8253e23c20f12981e902d25b1960a38cbe2e49cf
franzer <awfulrofl@gmail.com>
2024-11-30 22:56:59 -0700

    day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -40,20 +40,3 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     Some(total_matches.try_into().unwrap())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
